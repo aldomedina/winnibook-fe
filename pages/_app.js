6323d80963faf1741a7be9360646938409adf997
Aldo <aldo@studiographene.com>
2021-04-10 14:28:58 +0100

    theme provider added to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/globals.css";
 import "../styles/fonts.css";
+import Theme from "../components/Theme";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Theme>
+      <Component {...pageProps} />
+    </Theme>
+  );
 }
 
 export default MyApp;
