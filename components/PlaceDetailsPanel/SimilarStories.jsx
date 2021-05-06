e6b8817b279ed3781b94c6d226d797864f467873
Aldo <aldo@studiographene.com>
2021-05-06 11:55:07 +0100

    similar places title fixed, unused components removed and keys added to lists

--- components/PlaceDetailsPanel/SimilarStories.jsx
+++ components/PlaceDetailsPanel/SimilarStories.jsx
@@ -6,10 +6,10 @@ const SimilarStories = ({ stories }) => {
       <div className="min-h-50vh max-h-full gap-3 flex flex-col w-full">
         <h3 className="uppercase mb-3 font-medium">Featured In</h3>
         <div className="flex-1 flex flex-row h-full gap-5 md:gap-10">
-          {stories?.map(story => (
+          {stories?.map((story, i) => (
             <div className="flex-1 self-stretch">
               <StoryCard
-                key={story.id}
+                key={`${story.id}-${i}`}
                 image={story.image}
                 title={story.title}
                 categories={story.categories}
