ca680e0b1472b54bfd2f08bb93c1b607c335a93e
Thomás Agante <thomas@studiographene.com>
2021-05-18 12:28:10 +0100

    CMS

--- apollo/client.js
+++ apollo/client.js
@@ -3,9 +3,9 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 console.log(process.env.NEXT_PUBLIC_REACT_APP_FAUNA_SECRET);
 
 export const client = new ApolloClient({
-  uri: "https://graphql.fauna.com/graphql",
+  uri: "https://winnibook.hasura.app/v1/graphql",
   headers: {
-    authorization: `Bearer ${process.env.NEXT_PUBLIC_REACT_APP_FAUNA_SECRET}`,
+    "x-hasura-admin-secret": `5mKusFsywFpp4U86J42skwYreZLtj0SK3nf0zsLch3ecGJl4jH8twrpZu33YC62L`,
   },
   cache: new InMemoryCache(),
 });
---
+++ pages/admin/index.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react';
+
+const Admin = () => {
+
+  return (
+    <div className="">
+      
+    </div>
+  );
+};
+
+export default Admin;
