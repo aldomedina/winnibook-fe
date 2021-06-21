4ca00dbb253327d0d637f4b8db923c384aca824a
Aldo <aldo@studiographene.com>
2021-06-21 17:37:16 +0100

    dynamic meta theme implemented - so coooool

--- components/Head/index.jsx
+++ components/Head/index.jsx
@@ -1,9 +1,18 @@
 import Head from 'next/head';
-
-const CustomHead = ({ title }) => {
+import { useContext } from 'react';
+import { ColorContext } from '../Theme';
+import theme from '../Theme/colors';
+const CustomHead = ({ title, description }) => {
+  const { colorTheme } = useContext(ColorContext);
+  const color = colorTheme ? theme.colors[colorTheme].bg : theme.colors['base'].bg;
   return (
     <Head>
+      <meta name="theme-color" content={color} />
+      <meta charSet="UTF-8" />
       <title>{title ? `${title} | Winnibook` : 'Winnibook'}</title>
+      <meta name="description" content={description || ''} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" key="viewport" />
+
       <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
