4ea34e37b297a1b7c4b04d79ac364b321bc5f7b7
Thomás <thom.agante@gmail.com>
2021-06-23 18:28:53 +0100

    join auth fix

--- pages/api/join.js
+++ pages/api/join.js
@@ -1,7 +1,16 @@
-import { client } from '../../apollo/client';
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 import ADD_LOCAL from '../../apollo/mutations/local/insertJoin.gql';
 
 export default async function handler(req, res) {
+
+  const client = new ApolloClient({
+    uri: "https://winnibook.hasura.app/v1/graphql",
+    headers: {
+      "x-hasura-admin-secret": `5mKusFsywFpp4U86J42skwYreZLtj0SK3nf0zsLch3ecGJl4jH8twrpZu33YC62L`,
+    },
+    cache: new InMemoryCache(),
+  });
+
   if (req.method === 'POST') {
 
     const { data } = await client.mutate({
