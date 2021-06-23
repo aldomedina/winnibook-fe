7c1dda746a946aac184872395e70694295e9aee2
Thomás <thom.agante@gmail.com>
2021-06-23 18:32:31 +0100

    login return to

--- pages/api/auth/[...auth0].js
+++ pages/api/auth/[...auth0].js
@@ -5,6 +5,15 @@ const afterCallback = (req, res, session, state) => {
 };
 
 export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, {
+        returnTo: process.env.BASE_URL
+      });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  },
   async callback(req, res) {
     try {
       await handleCallback(req, res, { 
